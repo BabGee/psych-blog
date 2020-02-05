9e68f7be725d049ce77b2c4b2c964fb89cf7cac0
BabGee <gjeremiah170@gmail.com>
2020-02-05 12:04:24 +0300

    debug False

--- psychology/settings.py
+++ psychology/settings.py
@@ -24,9 +24,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = '0x!b#(1*cd73w$&azzc6p+essg7v=g80ls#z&xcx*mpemx&@9$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jeremih.loxpack.com', '127.0.0.1', '*']
 
 
 # Application definition
